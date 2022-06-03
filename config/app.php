d292ae2d2bdddc754a38620b44e426d6d243799f
Khuram Naqvi <anaqvi527@gmail.com>
2022-06-03 16:35:54 +0500

    Update app.php

--- config/app.php
+++ config/app.php
@@ -162,6 +162,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+       
+
         /*
          * Package Service Providers...
          */
@@ -232,6 +234,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Socialite' => Laravel\Socialite\Facades\Socialite::class,
+
 
 
 
